284e97b97569fdcfb0f8c80b2e0e657a481743b6
Kyle Geske <stungeye@gmail.com>
2022-09-29 14:50:03 -0500

    Added a Point and Ship struct. About to implement multiple ships in a vector.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,85 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	ssGlutton.avatar.load("Ships\\ship (3).png");
+	ssGlutton.coordinate.x = ofRandomWidth() - ssGlutton.avatar.getWidth();
+	ssGlutton.coordinate.y = ofRandomHeight() - ssGlutton.avatar.getHeight();
+	ssGlutton.speed.x = ofRandom(4) + 1;
+	ssGlutton.speed.y = ofRandom(4) + 1;
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	ssGlutton.coordinate.x += ssGlutton.speed.x;
+	ssGlutton.coordinate.y += ssGlutton.speed.y;
+
+	if (ssGlutton.coordinate.y > ofGetHeight() - ssGlutton.avatar.getHeight() || ssGlutton.coordinate.y < 0) {
+		ssGlutton.speed.y *= -1;
+	}
+
+	if (ssGlutton.coordinate.x > ofGetWidth() - ssGlutton.avatar.getWidth() || ssGlutton.coordinate.x < 0) {
+		ssGlutton.speed.x *= -1;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ssGlutton.avatar.draw(ssGlutton.coordinate.x, ssGlutton.coordinate.y);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,23 +2,35 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+struct Point {
+	int x{0};
+	int y{0};
+};
 
-	public:
-		void setup();
-		void update();
-		void draw();
+struct Ship {
+	Point coordinate;
+	Point speed;
+	ofImage avatar;
+};
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+class ofApp : public ofBaseApp {
+
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
+
+	void keyPressed(int key) override;
+	void keyReleased(int key) override;
+	void mouseMoved(int x, int y) override;
+	void mouseDragged(int x, int y, int button) override;
+	void mousePressed(int x, int y, int button) override;
+	void mouseReleased(int x, int y, int button) override;
+	void mouseEntered(int x, int y) override;
+	void mouseExited(int x, int y) override;
+	void windowResized(int w, int h) override;
+	void dragEvent(ofDragInfo dragInfo) override;
+	void gotMessage(ofMessage msg) override;
+
+	Ship ssGlutton;
 };
